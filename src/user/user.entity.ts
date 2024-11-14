101c44ee8676f3a9096c122d740db3b4ce18a836
Ankiith-M <ankith.m@roxiler.com>
2024-11-14 15:51:58 +0530

    Added typeorm and sqlite

---
+++ src/user/user.entity.ts
@@ -0,0 +1,11 @@
+import { Column, PrimaryGeneratedColumn, Entity } from "typeorm";
+
+@Entity()
+export class User{
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string
+  @Column()
+  password: string 
+}
